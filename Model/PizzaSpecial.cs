27c1aa43f1f662c6e04f531c9211ce09e54bf267
Jonathan Lorenzo Contrera <jonathanlorenzoc_rt@outlook.es>
2024-02-16 11:38:45 -0400

    feat: :sparkles: Culminación de la parte de interoperabilidad con Js

--- Model/PizzaSpecial.cs
+++ Model/PizzaSpecial.cs
@@ -15,6 +15,8 @@ public class PizzaSpecial
 
     public string ImageUrl { get; set; }
 
+    public int? FixedSize { get; set; }
+
     public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
 }
 
